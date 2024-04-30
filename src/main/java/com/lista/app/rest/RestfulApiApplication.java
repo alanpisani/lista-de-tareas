2db19bc4090f1f0e5735ccd91b4148ac55d21561
Alan Pisani <alanpisani1993@gmail.com>
2024-04-30 02:36:04 -0300

    Añadidas variables de entorno

--- src/main/java/com/lista/app/rest/RestfulApiApplication.java
+++ src/main/java/com/lista/app/rest/RestfulApiApplication.java
@@ -9,6 +9,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 import java.util.Set;
@@ -19,58 +22,18 @@ public class RestfulApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApiApplication.class, args);
 	}
-	@Bean
-	CommandLineRunner init(UserRepository userRepository) {
-		return args -> {
 
-			//CREATE PERMISSIONS
-			PermissionEntity createPermission = PermissionEntity.builder()
-					.name("CREATE")
-					.build();
-			PermissionEntity readPermission = PermissionEntity.builder()
-					.name("READ")
-					.build();
-			PermissionEntity updatePermission = PermissionEntity.builder()
-					.name("UPDATE")
-					.build();
-			PermissionEntity deletePermission = PermissionEntity.builder()
-					.name("DELETE")
-					.build();
-			PermissionEntity refactorPermission = PermissionEntity.builder()
-					.name("REFACTOR")
-					.build();
-
-			//CREAR ROLES
-
-			RoleEntity roleAdmin = RoleEntity.builder()
-					.roleEnum(RoleEnum.ADMIN)
-					.permissionsList(Set.of(createPermission, readPermission, updatePermission, deletePermission))
-					.build();
-			RoleEntity roleUser = RoleEntity.builder()
-					.roleEnum(RoleEnum.USER)
-					.permissionsList(Set.of(createPermission, readPermission, deletePermission))
-					.build();
-
-
-			UserEntity userAlan = UserEntity.builder()
-					.username("Alan")
-					.password("1234")
-					.isEnable(true)
-					.accountNoExpired(true)
-					.accountNoLocked(true)
-					.credentialNoExpired(true)
-					.roles(Set.of(roleAdmin))
-					.build();
-			UserEntity userDay = UserEntity.builder()
-					.username("Daiana")
-					.password("1234")
-					.isEnable(true)
-					.accountNoExpired(true)
-					.accountNoLocked(true)
-					.credentialNoExpired(true)
-					.roles(Set.of(roleAdmin))
-					.build();
-			userRepository.saveAll(List.of(userAlan, userDay));
-		};
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 }
